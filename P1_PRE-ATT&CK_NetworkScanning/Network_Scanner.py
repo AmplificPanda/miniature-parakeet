f73153062330b74213ad4554e9df74c81bd9507a
RTTGOD <avianrtt71@gmail.com>
2022-08-31 15:20:22 +1200

    2,000+ changes

---
+++ P1_PRE-ATT&CK_NetworkScanning/Network_Scanner.py
@@ -0,0 +1,5 @@
+from scapy.all import *
+#python -m venv venv - to make a virtual environment
+#pip install sklearn - sklearn/any other libary to install to venv
+#pip freeze > requirements.txt - to see what libraries are installed
+
