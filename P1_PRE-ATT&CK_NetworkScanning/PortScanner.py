b4072518c5ca09bffe2b5254b52ae1cb8b334d95
RTTGOD <avianrtt71@gmail.com>
2022-09-02 18:00:57 +1200

    port scanner using socket library(working)

--- P1_PRE-ATT&CK_NetworkScanning/PortScanner.py
+++ P1_PRE-ATT&CK_NetworkScanning/PortScanner.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3 
 #the above specifies which interpreter will be used to run the script.
-from tabnanny import verbose
-from scapy.all import *
+#from scapy.all import *
 import pyfiglet #for fancy graphics
 import sys #handling exceptions
 import socket #use for port, internet and more
@@ -35,38 +34,51 @@ print(ascii_banner)
 try:
     #allow user to specify target IP
     host = input(str("Enter target IP: "))
-    # for port in range(1,65535):
-    #     s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    #     socket.setdefaulttimeout(0.1)
+    
+    #show status to user
+    print("Program running scan on {}".format(host))
+    print("Scanning begun at: " + str(datetime.now()))
+    startTime=datetime.now()
 
-    #     #return the open ports
-    #     results = s.connect_ex((target,port))
+    #go through every port possible for the target IP
+    for port in range(1,65535):
+        #set up a socket and set its timeout to 0.1
+        s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        socket.setdefaulttimeout(0.1)
 
-    #     if results == 0:
-    #         #if 0 that means connection success
-    #         print("{} is open".format(port))
-    #     s.close() #close the socket
+        #return the open ports through sending a packet to each port in the IP
+        results = s.connect_ex((host,port))
 
-    #scapy approach:
-    def SynScanner(host):
-        #use the sr function
-        for port in range(80,445):
-            reply,noreply = sr(IP(dst=host)/TCP(dport=port, flags="S"),timeout=0.1,verbose=0)
+        if results == 0:
+            #if 0 that means connection success
+            print("{} is open".format(port))
+        s.close() #close the socket
+    endTime = datetime.now()
+    print("Scanning finished at: " + str(datetime.now()))
+    totalTime = endTime - startTime
+    print("Total scan duration: " + str(totalTime))
+
+
+    # #scapy approach: Currently a GitHub issue: #3714
+    # def SynScanner(host):
+    #     #use the sr function
+    #     for port in range(80,445):
+    #         reply,noreply = sr(IP(dst=host)/TCP(dport=port, flags="S"),timeout=0.1,verbose=0)
         
-        #output the responses
-        print(reply)
+    #     #output the responses
+    #     print(reply)
 
-        print("Open ports are:")
+    #     print("Open ports are:")
 
-        for(s,r) in reply:
-            print(s[TCP].dport)
+    #     for(s,r) in reply:
+    #         print(s[TCP].dport)
 
-    SynScanner(host)
+    # SynScanner(host)
 
 except KeyboardInterrupt:
-    print("Keyboard interupt, exiting now...")
+    print("Keyboard interupt detected, exiting now...")
     sys.exit()
 
 except socket.error:
-    print("host not responding")
+    print("host not responding :(")
     sys.exit()
