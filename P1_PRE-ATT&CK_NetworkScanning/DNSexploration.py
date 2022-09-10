084711dff55cc8d6712263b369bea4dfa69cdd4f
RTTGOD <avianrtt71@gmail.com>
2022-09-11 00:38:02 +1200

    added a bit more skeleton

--- P1_PRE-ATT&CK_NetworkScanning/DNSexploration.py
+++ P1_PRE-ATT&CK_NetworkScanning/DNSexploration.py
@@ -1,5 +1,22 @@
 #!/usr/bin/python
 
 #This is for the DNS Exploration segment in Week 2
-
+#----------------------------------------------------
+#for python reading from a textfile:
+#https://www.pythontutorial.net/python-basics/python-read-text-file/
+#----------------------------------------------------
 #set up target domain:
+domain = "www.google.com"
+searchlist = "subdomains.txt"
+
+#create empty dictionary to hold all the subs within the txt file
+dictionary = [] 
+
+#read mode 
+with open(domain,"r") as f:
+    dictionary = f.read().splitlines()
+
+def DomainSearcher():
+    print("To implement")
+
+DomainSearcher(domain,dictionary,True)
